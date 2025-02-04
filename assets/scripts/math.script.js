929d3399f56f819e82e2ca02eb83397c2a75832c
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-02-04 10:27:05 -0300

    feat: update calculate function

--- assets/scripts/math.script.js
+++ assets/scripts/math.script.js
@@ -4,10 +4,40 @@ function calcular() {
 
     //alert(numA)
     //alert(numB)
+}
 
-    //Soma
+function soma() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+    
+    let result = numA + numB
+    
+    document.getElementById("result").innerText = result;
+}
 
-    let result = numA + numB 
+function sub() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+    
+    let result = numA - numB
+    
+    document.getElementById("result").innerText = result;
+}
 
+function multi() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+    
+    let result = numA * numB
+    
+    document.getElementById("result").innerText = result;
+}
+
+function div() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+    
+    let result = numA / numB
+    
     document.getElementById("result").innerText = result;
 }
